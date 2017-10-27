45142bf0f741022cb599a5afea2ee9991484ab82
daizhe0417 <daizhe0417@163.com>
2017-10-27 20:49:34 +0800

    test tag

--- app/src/main/java/cn/daizhe/opencv4androiddemo/MainActivity.java
+++ app/src/main/java/cn/daizhe/opencv4androiddemo/MainActivity.java
@@ -304,82 +304,82 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         int height = rows * 3 / 4;
 
         switch (Cur_State) {
-            case 1:
-                //灰化处理
-                Imgproc.cvtColor(inputFrame.gray(), mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
-                break;
-            case 2:
-                //Canny边缘检测
-                mRgba = inputFrame.rgba();
-                Imgproc.Canny(inputFrame.gray(), mTmp, 80, 100);
-                Imgproc.cvtColor(mTmp, mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
-                break;
-            case 3:
-                //Hist直方图计算
-                Mat hist = new Mat();
-                int thikness = (int) (sizeRgba.width / (mHistSizeNum + 10) / 5);
-                if (thikness > 5) thikness = 5;
-                int offset = (int) ((sizeRgba.width - (5 * mHistSizeNum + 4 * 10) * thikness) / 2);
-
-                // RGB
-                for (int c = 0; c < 3; c++) {
-                    Imgproc.calcHist(Arrays.asList(mRgba), mChannels[c], mMat0, hist, mHistSize, mRanges);
-                    Core.normalize(hist, hist, sizeRgba.height / 2, 0, Core.NORM_INF);
-                    hist.get(0, 0, mBuff);
-                    for (int h = 0; h < mHistSizeNum; h++) {
-                        mP1.x = mP2.x = offset + (c * (mHistSizeNum + 10) + h) * thikness;
-                        mP1.y = sizeRgba.height - 1;
-                        mP2.y = mP1.y - 2 - (int) mBuff[h];
-                        Imgproc.line(mRgba, mP1, mP2, mColorsRGB[c], thikness);
-                    }
-                }
-                // Value and Hue
-                Imgproc.cvtColor(mRgba, mTmp, Imgproc.COLOR_RGB2HSV_FULL);
-                // Value
-                Imgproc.calcHist(Arrays.asList(mTmp), mChannels[2], mMat0, hist, mHistSize, mRanges);
-                Core.normalize(hist, hist, sizeRgba.height / 2, 0, Core.NORM_INF);
-                hist.get(0, 0, mBuff);
-                for (int h = 0; h < mHistSizeNum; h++) {
-                    mP1.x = mP2.x = offset + (3 * (mHistSizeNum + 10) + h) * thikness;
-                    mP1.y = sizeRgba.height - 1;
-                    mP2.y = mP1.y - 2 - (int) mBuff[h];
-                    Imgproc.line(mRgba, mP1, mP2, mWhilte, thikness);
-                }
-                break;
-            case 4:
-                //Sobel边缘检测
-                Mat gray = inputFrame.gray();
-                Mat grayInnerWindow = gray.submat(top, top + height, left, left + width);
-                rgbaInnerWindow = mRgba.submat(top, top + height, left, left + width);
-                Imgproc.Sobel(grayInnerWindow, mIntermediateMat, CvType.CV_8U, 1, 1);
-                Core.convertScaleAbs(mIntermediateMat, mIntermediateMat, 10, 0);
-                Imgproc.cvtColor(mIntermediateMat, rgbaInnerWindow, Imgproc.COLOR_GRAY2BGRA, 4);
-                grayInnerWindow.release();
-                rgbaInnerWindow.release();
-                break;
-            case 5:
-                //SEPIA(色调变换)
-                rgbaInnerWindow = mRgba.submat(top, top + height, left, left + width);
-                Core.transform(rgbaInnerWindow, rgbaInnerWindow, mSepiaKernel);
-                rgbaInnerWindow.release();
-                break;
-            case 6:
-                //ZOOM放大镜
-                Mat zoomCorner = mRgba.submat(0, rows / 2 - rows / 10, 0, cols / 2 - cols / 10);
-                Mat mZoomWindow = mRgba.submat(rows / 2 - 9 * rows / 100, rows / 2 + 9 * rows / 100, cols / 2 - 9 * cols / 100, cols / 2 + 9 * cols / 100);
-                Imgproc.resize(mZoomWindow, zoomCorner, zoomCorner.size());
-                Size wsize = mZoomWindow.size();
-                Imgproc.rectangle(mZoomWindow, new Point(1, 1), new Point(wsize.width - 2, wsize.height - 2), new Scalar(255, 0, 0, 255), 2);
-                zoomCorner.release();
-                mZoomWindow.release();
-                break;
-            case 7:
-                //PIXELIZE像素化
-                rgbaInnerWindow = mRgba.submat(top, top + height, left, left + width);
-                Imgproc.resize(rgbaInnerWindow, mIntermediateMat, mSize0, 0.1, 0.1, Imgproc.INTER_NEAREST);
-                Imgproc.resize(mIntermediateMat, rgbaInnerWindow, rgbaInnerWindow.size(), 0., 0., Imgproc.INTER_NEAREST);
-                rgbaInnerWindow.release();
-                break;
+            //case 1:
+            //    //灰化处理
+            //    Imgproc.cvtColor(inputFrame.gray(), mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
+            //    break;
+            //case 2:
+            //    //Canny边缘检测
+            //    mRgba = inputFrame.rgba();
+            //    Imgproc.Canny(inputFrame.gray(), mTmp, 80, 100);
+            //    Imgproc.cvtColor(mTmp, mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
+            //    break;
+            //case 3:
+            //    //Hist直方图计算
+            //    Mat hist = new Mat();
+            //    int thikness = (int) (sizeRgba.width / (mHistSizeNum + 10) / 5);
+            //    if (thikness > 5) thikness = 5;
+            //    int offset = (int) ((sizeRgba.width - (5 * mHistSizeNum + 4 * 10) * thikness) / 2);
+            //
+            //    // RGB
+            //    for (int c = 0; c < 3; c++) {
+            //        Imgproc.calcHist(Arrays.asList(mRgba), mChannels[c], mMat0, hist, mHistSize, mRanges);
+            //        Core.normalize(hist, hist, sizeRgba.height / 2, 0, Core.NORM_INF);
+            //        hist.get(0, 0, mBuff);
+            //        for (int h = 0; h < mHistSizeNum; h++) {
+            //            mP1.x = mP2.x = offset + (c * (mHistSizeNum + 10) + h) * thikness;
+            //            mP1.y = sizeRgba.height - 1;
+            //            mP2.y = mP1.y - 2 - (int) mBuff[h];
+            //            Imgproc.line(mRgba, mP1, mP2, mColorsRGB[c], thikness);
+            //        }
+            //    }
+            //    // Value and Hue
+            //    Imgproc.cvtColor(mRgba, mTmp, Imgproc.COLOR_RGB2HSV_FULL);
+            //    // Value
+            //    Imgproc.calcHist(Arrays.asList(mTmp), mChannels[2], mMat0, hist, mHistSize, mRanges);
+            //    Core.normalize(hist, hist, sizeRgba.height / 2, 0, Core.NORM_INF);
+            //    hist.get(0, 0, mBuff);
+            //    for (int h = 0; h < mHistSizeNum; h++) {
+            //        mP1.x = mP2.x = offset + (3 * (mHistSizeNum + 10) + h) * thikness;
+            //        mP1.y = sizeRgba.height - 1;
+            //        mP2.y = mP1.y - 2 - (int) mBuff[h];
+            //        Imgproc.line(mRgba, mP1, mP2, mWhilte, thikness);
+            //    }
+            //    break;
+            //case 4:
+            //    //Sobel边缘检测
+            //    Mat gray = inputFrame.gray();
+            //    Mat grayInnerWindow = gray.submat(top, top + height, left, left + width);
+            //    rgbaInnerWindow = mRgba.submat(top, top + height, left, left + width);
+            //    Imgproc.Sobel(grayInnerWindow, mIntermediateMat, CvType.CV_8U, 1, 1);
+            //    Core.convertScaleAbs(mIntermediateMat, mIntermediateMat, 10, 0);
+            //    Imgproc.cvtColor(mIntermediateMat, rgbaInnerWindow, Imgproc.COLOR_GRAY2BGRA, 4);
+            //    grayInnerWindow.release();
+            //    rgbaInnerWindow.release();
+            //    break;
+            //case 5:
+            //    //SEPIA(色调变换)
+            //    rgbaInnerWindow = mRgba.submat(top, top + height, left, left + width);
+            //    Core.transform(rgbaInnerWindow, rgbaInnerWindow, mSepiaKernel);
+            //    rgbaInnerWindow.release();
+            //    break;
+            //case 6:
+            //    //ZOOM放大镜
+            //    Mat zoomCorner = mRgba.submat(0, rows / 2 - rows / 10, 0, cols / 2 - cols / 10);
+            //    Mat mZoomWindow = mRgba.submat(rows / 2 - 9 * rows / 100, rows / 2 + 9 * rows / 100, cols / 2 - 9 * cols / 100, cols / 2 + 9 * cols / 100);
+            //    Imgproc.resize(mZoomWindow, zoomCorner, zoomCorner.size());
+            //    Size wsize = mZoomWindow.size();
+            //    Imgproc.rectangle(mZoomWindow, new Point(1, 1), new Point(wsize.width - 2, wsize.height - 2), new Scalar(255, 0, 0, 255), 2);
+            //    zoomCorner.release();
+            //    mZoomWindow.release();
+            //    break;
+            //case 7:
+            //    //PIXELIZE像素化
+            //    rgbaInnerWindow = mRgba.submat(top, top + height, left, left + width);
+            //    Imgproc.resize(rgbaInnerWindow, mIntermediateMat, mSize0, 0.1, 0.1, Imgproc.INTER_NEAREST);
+            //    Imgproc.resize(mIntermediateMat, rgbaInnerWindow, rgbaInnerWindow.size(), 0., 0., Imgproc.INTER_NEAREST);
+            //    rgbaInnerWindow.release();
+            //    break;
             default:
                 //显示原图
                 mRgba = inputFrame.rgba();
